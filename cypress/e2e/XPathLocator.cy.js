e469ead4fe91c1a3936cd97241bfe87ccd630804
manojbitty-star <manojbitty@gmail.com>
2026-02-21 17:25:26 +0530

    Modified XPatchLocator

--- cypress/e2e/XPathLocator.cy.js
+++ cypress/e2e/XPathLocator.cy.js
@@ -3,7 +3,7 @@ describe('Xpath Locator',()=>
         it('Find No of Products on the page using Xpath Locator',()=>{
             cy.visit("http://www.automationpractice.pl/")
             cy.get(".sf-with-ul[title='Women']").click()
-            cy.xpath("//ul[@class='product_list grid row']/li").should('have.length',7)
+            cy.xpath("//ul[@class='product_list grid row']/li").should('have.length',8)
         })
 
 })
